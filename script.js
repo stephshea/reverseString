9d76e52ca4ee930bf88c051d99c85cf098cb41b8
Stephanie Shea <stephcshea@gmail.com>
2017-11-25 19:28:38 +0000

    Tidied code

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 //5 versions of reverseString script
 //shortest to longest
-
 //1 shortest
 function r(s) {
-  return s.split('').reverse().join('');
-   } 
+	return s.split('').reverse().join('');
+}
 r("hello");
-
 //2
 // function reverseString(str) {
 //   return str.split("").reverse().join("");
 //   }
 // reverseString("hello");
-
-
 // 3
 // function reverseString(s) {
 // 	var r = s.split("").reverse().join("");
 // 	return r;
 // }
 // reverseString("hello");
-
 //4
 // function reverseString(str) {
 //   str = str.split("");
-  
 //   str.reverse();
-  
 //   return str.join("");
 //   }
-
 //5
 // function reverseString(str) {
 //   var strArray = str.split("");
-  
 //   strArray.reverse();
-  
 //   var reversedStr = strArray.join("");
-  
 //   return reversedStr;
 // }
-
 // reverseString("hello");
